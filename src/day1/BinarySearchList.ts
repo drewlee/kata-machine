c9e896ea46670402e14976ae2507cff9b1caa46a
Andrew A Lee <andrew@andrewleedev.com>
2026-01-24 16:12:57 -0800

    Completes first 3 day 1 exercises

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  let low = 0;
+  let high = haystack.length;
 
+  while (low < high) {
+    const mid = low + Math.floor((high - low) / 2);
+    const value = haystack[mid];
+
+    if (value === needle) {
+      return true;
+    } else if (value > needle) {
+      high = mid;
+    } else { // value < needle
+      low = mid + 1;
+    }
+  }
+
+  return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,24 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
+  let i = jumpAmount;
 
+  while (i < breaks.length) {
+    if (breaks[i]) {
+      break;
+    }
+
+    i += jumpAmount;
+  }
+
+  let j = i - jumpAmount;
+
+  while (j <= i) {
+    if (breaks[j]) {
+      return j;
+    }
+
+    j++;
+  }
+
+  return -1;
 }
