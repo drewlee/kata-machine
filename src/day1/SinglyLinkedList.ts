5ec01594ebc534d51517bdbd4a8c6b72e3dd8c86
Andrew A Lee <andrew@andrewleedev.com>
2026-01-25 19:47:12 -0800

    Completes singly linked lists

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,178 @@
+class Node<T> {
+  value: T;
+  next?: Node<T>;
+
+  constructor(value: T, next?: Node<T>) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
 export default class SinglyLinkedList<T> {
-    public length: number;
+  private head: Node<T> | undefined;
+  public length: number;
 
-    
+  constructor() {
+    this.length = 0;
+  }
 
-    constructor() {
+  /**
+   * Inserts an item at the beginning of the list.
+   *
+   * @param item - Value to insert.
+   */
+  prepend(item: T): void {
+    const newNode = new Node(item);
+
+    newNode.next = this.head;
+    this.head = newNode;
+    this.length++;
+  }
+
+  /**
+   * Inserts an item into the list at the specified index.
+   *
+   * @param item - Value to insert.
+   * @param idx - Index to insert the value at.
+   */
+  insertAt(item: T, idx: number): void {
+    if (idx === 0) {
+      this.prepend(item);
+      return;
     }
 
-    prepend(item: T): void {
+    if (!this.head) {
+      return;
+    }
 
-}
-    insertAt(item: T, idx: number): void {
+    let currNode = this.head;
+    let currIdx = 0;
 
-}
-    append(item: T): void {
+    while (currNode.next && idx !== currIdx + 1) {
+      currNode = currNode.next;
+      currIdx++;
+    }
 
-}
-    remove(item: T): T | undefined {
+    const newNode = new Node(item);
+    const tmp = currNode.next?.next;
 
-}
-    get(idx: number): T | undefined {
+    currNode.next = newNode;
+    newNode.next = tmp;
+    this.length++;
+  }
 
-}
-    removeAt(idx: number): T | undefined {
+  /**
+   * Inserts an item at the end of the list.
+   *
+   * @param item - Value to insert.
+   */
+  append(item: T): void {
+    const newNode = new Node(item);
+    this.length++;
 
-}
+    if (!this.head) {
+      this.head = newNode;
+      return;
+    }
+
+    let currNode = this.head;
+
+    while (currNode.next) {
+      currNode = currNode.next;
+    }
+
+    currNode.next = newNode;
+  }
+
+  /**
+   * Removes the specified value from the list.
+   *
+   * @param item - Value to remove.
+   * @returns The removed value.
+   */
+  remove(item: T): T | undefined {
+    if (!this.head) {
+      return;
+    }
+
+    if (this.head.value === item) {
+      return this.removeAt(0);
+    }
+
+    let currNode = this.head;
+    let result: T | undefined = undefined;
+
+    while (currNode.next) {
+      if (currNode.next.value === item) {
+        const targetNode = currNode.next;
+
+        currNode.next = targetNode.next;
+        result = targetNode.value;
+        this.length--;
+
+        break;
+      }
+
+      currNode = currNode.next;
+    }
+
+    return result;
+  }
+
+  /**
+   * Gets the value at the specified index.
+   *
+   * @param idx - Index to get the value for.
+   * @returns The value.
+   */
+  get(idx: number): T | undefined {
+    let currNode = this.head;
+    let currIdx = 0;
+
+    while (currNode && currIdx !== idx) {
+      currNode = currNode.next;
+      currIdx++;
+    }
+
+    return currNode?.value;
+  }
+
+  /**
+   * Removes the value at the specified index.
+   *
+   * @param idx - Index to remove the value at.
+   * @returns The removed value, or `undefined` if the index is out of bounds.
+   */
+  removeAt(idx: number): T | undefined {
+    if (!this.head) {
+      return;
+    }
+
+    if (idx === 0) {
+      const targetNode = this.head;
+      this.head = targetNode.next;
+      this.length--;
+
+      return targetNode.value;
+    }
+
+    let currNode = this.head;
+    let currIdx = 0;
+
+    while (currNode.next && idx !== currIdx + 1) {
+      currNode = currNode.next;
+      currIdx++;
+    }
+
+    // The index is out of bounds.
+    if (currIdx > idx) {
+      return;
+    }
+
+    const targetNode = currNode.next!;
+    currNode.next = targetNode.next;
+    this.length--;
+
+    return targetNode.value;
+  }
 }
