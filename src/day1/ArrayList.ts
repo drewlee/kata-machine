973d7ed3da1ccda8468ad034df9ed5826894f58a
Andrew A Lee <andrew@andrewleedev.com>
2026-02-05 22:26:46 -0800

    Completes Array List

--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,119 @@
 export default class ArrayList<T> {
-    public length: number;
+  public length: number;
+  private array: T[];
+  private capacity: number;
 
-    
+  constructor(capacity = 5) {
+    this.capacity = capacity;
+    this.array = new Array(capacity);
+    this.length = 0;
+  }
 
-    constructor() {
+  /**
+   * Increases the array list's capacity when it is about to be exceeded.
+   */
+  private checkCapacity() {
+    if (this.length < this.capacity) {
+      return;
     }
 
-    prepend(item: T): void {
+    const oldArray = this.array;
+    this.capacity *= 2;
+    this.array = new Array(this.capacity);
 
-}
-    insertAt(item: T, idx: number): void {
+    for (let i = 0; i < this.length; i++) {
+      this.array[i] = oldArray[i];
+    }
+  }
 
-}
-    append(item: T): void {
+  /**
+   * Adds a new item to the beginning of the array list.
+   *
+   * @param item - Item to add.
+   */
+  prepend(item: T): void {
+    this.insertAt(item, 0);
+  }
 
-}
-    remove(item: T): T | undefined {
+  /**
+   * Inserts an item at the specified index.
+   *
+   * @param item - Item to add.
+   * @param idx - Insertion index.
+   */
+  insertAt(item: T, idx: number): void {
+    if (idx < 0 || idx > this.length) {
+      return;
+    }
+    this.checkCapacity();
 
-}
-    get(idx: number): T | undefined {
+    // Shift items right
+    for (let i = this.length; i > idx; i--) {
+      this.array[i] = this.array[i - 1];
+    }
+    this.array[idx] = item;
+    this.length++;
+  }
 
-}
-    removeAt(idx: number): T | undefined {
+  /**
+   * Adds a new item to the end of the array list.
+   *
+   * @param item - Item to add.
+   */
+  append(item: T): void {
+    this.checkCapacity();
+    this.array[this.length] = item;
+    this.length++;
+  }
 
-}
+  /**
+   * Removes the specified item from the array list.
+   *
+   * @param item - Item to remove.
+   * @returns Removed item.
+   */
+  remove(item: T): T | undefined {
+    let idx = 0;
+    while (idx < this.length && item !== this.array[idx]) {
+      idx++;
+    }
+
+    // Item not found
+    if (idx === this.length) {
+      return;
+    }
+
+    return this.removeAt(idx);
+  }
+
+  /**
+   * Retrieves the item stored at the specified index.
+   *
+   * @param idx - Numeric index.
+   * @returns The item.
+   */
+  get(idx: number): T | undefined {
+    return this.array[idx];
+  }
+
+  /**
+   * Removes and returns the item at the specified index.
+   *
+   * @param idx - Numeric index.
+   * @returns The item.
+   */
+  removeAt(idx: number): T | undefined {
+    if (idx < 0 || idx >= this.length) {
+      return;
+    }
+
+    const item = this.array[idx];
+    for (let i = idx; i < this.length - 1; i++) {
+      this.array[i] = this.array[i + 1];
+    }
+
+    this.length--;
+    delete this.array[this.length];
+    return item;
+  }
 }
