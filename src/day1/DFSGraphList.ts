d50eab0e6bd252c0ae0142d854150143f6d64b12
Andrew A Lee <andrew@andrewleedev.com>
2026-02-09 15:59:07 -0800

    Completes DFS Graph List

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,53 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+  graph: WeightedAdjacencyList,
+  curr: number,
+  needle: number,
+  seen: boolean[],
+  path: number[]
+): boolean {
+  if (seen[curr]) {
+    return false;
+  }
 
+  seen[curr] = true;
+
+  // Pre
+  path.push(curr);
+
+  if (curr === needle) {
+    return true;
+  }
+
+  // Recurse
+  const list = graph[curr];
+
+  for (let i = 0; i < list.length; i++) {
+    const edge = list[i];
+
+    if (walk(graph, edge.to, needle, seen, path)) {
+      return true;
+    }
+  }
+
+  // Post
+  path.pop();
+
+  return false;
+}
+
+export default function dfs(
+  graph: WeightedAdjacencyList,
+  source: number,
+  needle: number
+): number[] | null {
+  const seen: boolean[] = new Array(graph.length).fill(false);
+  const path: number[] = [];
+
+  walk(graph, source, needle, seen, path);
+
+  if (path.length) {
+    return path;
+  }
+
+  return null;
 }
